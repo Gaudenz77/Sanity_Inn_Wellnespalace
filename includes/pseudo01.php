903c383f53662fa36dcc4102af4eb6d6bd4259fd
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2023-01-16 09:15:12 +0100

    change
    change

---
+++ includes/pseudo01.php
@@ -0,0 +1,82 @@
+<?php
+$questions = array(
+    "question01" => array(
+        "question" => "Wie gesund bist du körperlich?",
+        /* id => question01, */
+        "type" => "Slider",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+    "question02" => array(
+        "question" => " Nimmst du Nahrungsergänzungsmittel?",
+        "type" => "Slider",
+        "value" => array("ja","nein")
+    ),
+    "question03" => array(
+        "question" => "Wie wichtig ist köperliche Aktivität für dich?",
+        "type" => "Slider",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+    "question04" => array(
+        "question" => "Welche zusätzliche körperliche Aktivität betreibst du am meisten?",
+        "type" => "Slider",
+        "value" => array(
+            "Gewichte heben",
+            "Gehen",
+            "Wandern",
+            "Joggen",
+            "Rennen",
+            "Schwimmen",
+            "Tanzen",
+            "Aerobics",
+            "Pilates",
+            "Team Sport",
+            "Andere"
+        )
+    ),
+    "question05" => array(
+        "question" => "Hast du das Gefühl, zu wenig, genügend oder viel zu viel zusätzliche körperliche Aktivitäten zu betreiben?",
+        "type" => "Slider",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+    "question06" => array(
+        "question" => "An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Kohlenhydrate?",
+        "type" => "Number",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+    "question07" => array(
+        "question" => " An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Protein?",
+        "type" => "Number",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+    "question08" => array(
+        "question" => "An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Gemüse?",
+        "type" => "Number",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+    "question09" => array(
+        "question" => "An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Früchte?",
+        "type" => "Number",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+    "question10" => array(
+        "question" => "An einem typischen Tag: Wie viele deiner Malzeiten kommen aus der Mikrowelle oder sind schon fertig zubereitet?",
+        "min" => 1,
+        "max" => 5,
+        "value" => 3
+    ),
+)
+?>
