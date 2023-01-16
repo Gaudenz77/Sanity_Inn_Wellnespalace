2a22336ad3276af750a801a31224c53b9dae637d
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2023-01-16 09:38:43 +0100

    haed include

---
+++ includes/haed.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css%22%3E
+    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lexend+Deca:wght@100;300;400;500;700;900&family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d4cbcb96c8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico">
+    <script src="./js/main.js" defer></script>
+    <script src="./js/confirmation.js"></script>
+    <title>SANITY-INN-Wellness-Palace</title>
+    <style>
+      body {
+        margin-top:18%;
+        background: url('');
+        width: 100%;
+        height: auto;
+        background-size:cover;
+        background-repeat: no-repeat;
+        font-family: 'Lexend Deca', sans-serif;
+      }
+
+
+</style>
+
+  </head>
