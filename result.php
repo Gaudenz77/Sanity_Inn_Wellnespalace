ad7f81b0055c8bc30263e7ff20c4bea3bc204354
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-01-18 16:46:44 +0100

    basic table

--- result.php
+++ result.php
@@ -1,5 +1,5 @@
 <?php 
-require 'includes/session.php';
+/* require 'includes/session.php'; */
 ?>
 <body>
 <main>
@@ -18,18 +18,54 @@ require 'includes/session.php';
         require 'includes/pseudo01.php';
         require 'includes/header.php';
         $pageid =  9;
-        require 'includes/phpValidation.php';
+        /* require 'includes/phpValidation.php'; */
         $prüfung = 'hacker';
         switch($prüfung){
           case 'hacker':
-            echo "<p id='validateAllquestion'>Wenn du nicht richtig ausfüllst können wir dich auch nicht richtig beraten!</p>";
+            echo "<p id='validateAllquestion'>Wenn Du nicht richtig ausfüllst, können wir Dich auch nicht richtig beraten!</p>";
           case 'helthy':
-            echo "<p id='validateAllquestion'>Super du bist kern gesund wir empfehle dieses Abo demit das auch so bleibt.</p>";
+            echo "<p id='validateAllquestion'>Super, Du bist kern gesund! Wir empfehlen das Top-Abo, damit das auch so bleibt.</p>";
           case 'unhalthi':
-            echo "<p id='validateAllquestion'>Wir empfehle dieses Abo demit das auch so bleibt.</p>";
+            echo "<p id='validateAllquestion'>Wir empfehlen das Top-Abo, damit das auch so bleibt./p>";
         }
 
-        
+        echo "<div class='container'>
+        <div class='row justify-content-center'>
+            <div class='col-sm bg-secondary p-2'>
+                <div class='one'>Wenn Du nicht richtig ausfüllst, können wir Dich auch nicht richtig beraten!</div>
+                <div class='one'>Super, Du bist kern gesund! Wir empfehlen das Top-Abo, damit das auch so bleibt.</div>
+                <div class='one'>Wir empfehlen das Top-Abo, damit das auch so bleibt.</div>
+            </div>
+            <div class='col-sm bg-secondary p-2'>
+            <div class='table-responsive'>
+                <table class='table table-dark table-striped'>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Frage No. X</th>
+                            <th scope='col'></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class=''>
+                            <td scope='row'>Frage No. 1</td>
+                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, aliquid voluptates eum est amet hic rem esse numquam laborum consequatur vel mollitia dolorem possimus ab impedit aperiam quas et sequi.
+                            </td>
+                        </tr>
+                        <tr class=''>
+                            <td scope='row'>Frage No. 2</td>
+                            <td>Antwort No. 2</td>
+                        </tr>
+                        <tr class=''>
+                            <td scope='row'>Frage No. 2</td>
+                            <td>Antwort No. 2</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+            </div>
+        </div>
+    </div>";
 
 
 
@@ -37,7 +73,7 @@ require 'includes/session.php';
 
         ?>
 
-        
+
 
           
           
